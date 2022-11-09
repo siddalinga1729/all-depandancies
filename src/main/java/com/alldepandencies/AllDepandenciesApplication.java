fb8c01c58835c6b06cc44be1efb25ddac74a680d
SIDDU <siddalinga1729@gmail.com>
2022-11-09 16:39:24 +0530

    all depandancies added

---
+++ src/main/java/com/alldepandencies/AllDepandenciesApplication.java
@@ -0,0 +1,13 @@
+package com.alldepandencies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllDepandenciesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllDepandenciesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alldepandencies/AllDepandenciesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alldepandencies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllDepandenciesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
